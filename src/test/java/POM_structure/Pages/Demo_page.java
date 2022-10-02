6c2a3856200accb0db8fd52530073d82a03a5d7c
viet12345 <vietdh42@wru.vn>
2022-10-02 19:14:56 +0700

    Setup Allure report

--- src/test/java/POM_structure/Pages/Demo_page.java
+++
@@ -1,8 +0,0 @@
-package POM_structure.Pages;
-
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-
-public class Demo_page {
-    @FindBy(name = "email") private WebElement email_locator;
-}
